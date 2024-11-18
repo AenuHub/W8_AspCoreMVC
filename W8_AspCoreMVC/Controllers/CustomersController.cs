df360bb4e5370f103f5f32fb9edd04f62121b03e
AenuHub <jesuskane92@gmail.com>
2024-11-18 14:57:06 +0300

    create CustomersController.cs

---
+++ W8_AspCoreMVC/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using W8_AspCoreMVC.Models;
+
+namespace W8_AspCoreMVC.Controllers;
+
+public class CustomersController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        var customer = new Customer
+        {
+            Id = 1,
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com"
+        };
+
+        var viewModel = new CustomerViewModel
+        {
+            Customer = customer,
+            WelcomeMessage = "Welcome to our customer portal"
+        };
+        
+        return View(viewModel);
+    }
+}
