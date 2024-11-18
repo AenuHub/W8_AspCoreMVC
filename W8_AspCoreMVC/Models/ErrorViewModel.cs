75b70bb6f03841b7a04ae939a88fd11f973a1988
AenuHub <jesuskane92@gmail.com>
2024-11-18 14:41:21 +0300

    Initial commit

---
+++ W8_AspCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace W8_AspCoreMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
